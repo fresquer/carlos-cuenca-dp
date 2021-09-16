47f024d779ece086ceeda622261988b94f4993cb
Fernando Fresquer <fernando@fresquer.dev>
2021-09-16 10:07:35 +0200

    bye bye normalize

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
 const postcssPresetEnv = require(`postcss-preset-env`)
 const postcssImport = require(`postcss-import`)
-const postcssNormalize = require('postcss-normalize')
 
 module.exports = () => ({
     plugins: [
         postcssImport(),
-        postcssNormalize(),
         postcssPresetEnv({
             stage: 0,
         }),
